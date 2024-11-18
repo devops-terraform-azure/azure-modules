12508b6dda99aafa8b72c3abd53d95859ed54003
developer <ukohae39@gmail>
2024-11-18 00:07:47 -0500

    subscription id

--- provider.tf
+++ provider.tf
@@ -18,4 +18,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = var.AZURE_SUBSCRIPTION_ID
+  tenant_id       = var.AZURE_TENANT_ID
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
 }
