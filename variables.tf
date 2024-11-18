b9530667fdcba98ccaae70e1957cf1ee8b0e51b7
developer <ukohae39@gmail>
2024-11-17 22:49:38 -0500

    test terraform plan

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
-variable "AZURE_CLIENT_ID" {
-  description = "The Client ID"
-  type        = string
-}
+# variable "AZURE_CLIENT_ID" {
+#   description = "The Client ID"
+#   type        = string
+# }
 
-variable "AZURE_TENANT_ID" {
-  description = "The Tenant ID"
-  type        = string
-}
+# variable "AZURE_TENANT_ID" {
+#   description = "The Tenant ID"
+#   type        = string
+# }
 
-variable "AZURE_SUBSCRIPTION_ID" {
-  description = "The Subscription ID"
-  type        = string
-}
+# variable "AZURE_SUBSCRIPTION_ID" {
+#   description = "The Subscription ID"
+#   type        = string
+# }
 
-variable "AZURE_CLIENT_SECRET" {
-  description = "The Client Secrets"
-  type        = string
-}
+# variable "AZURE_CLIENT_SECRET" {
+#   description = "The Client Secrets"
+#   type        = string
+# }
