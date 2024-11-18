62d58fcd277ab1dfdd99f132f208dc6e9781679e
developer <ukohae39@gmail>
2024-11-17 23:35:16 -0500

    TF Cloud API Key

--- provider.tf
+++ provider.tf
@@ -16,12 +16,6 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-
-  }
-  # client_id       = var.AZURE_CLIENT_ID
-  # tenant_id       = var.AZURE_TENANT_ID
-  # subscription_id = var.AZURE_SUBSCRIPTION_ID
-  # client_secret   = var.AZURE_CLIENT_SECRET
-  subscription_id = var.ARM_SUBSCRIPTION_ID
+  features {}
+  subscription_id = ""
 }
